8618c9a89e7ff9043c416a4ef7571bc681156bf5
scmc24 <manuelsokoudjou@gmail.com>
2024-07-08 03:23:02 +0100

    ajout de commandes dans le start.sh pour les migrations

--- app/start.sh
+++ app/start.sh
@@ -11,13 +11,31 @@ then
     echo "PostgreSQL started"
 fi
 echo "========================makemigrations ========================"
-python manage.py makemigrations
-#python manage.py makemigrations accounts 
+echo "=============== makemigrations accounts ===============\n"
+python manage.py makemigrations accounts
+echo "=============== makemigrations chat ===============\n"
+python manage.py makemigrations chat
+echo "=============== makemigrations test ===============\n"
+python manage.py makemigrations test
+echo "=============== makemigrations course ===============\n"
+python manage.py makemigrations course
+echo "=============== makemigrations challenge ===============\n"
+python manage.py makemigrations challenge
 
 
-echo "================= migrate ====================================="
-echo "==============================================================="
-python manage.py migrate
+
+echo "======================== migrations ========================"
+echo "=============== migrate ===============\n"
+python manage.py migrate accounts
+echo "=============== migrate chat ===============\n"
+python manage.py migrate chat
+echo "=============== migrate test ===============\n"
+python manage.py migrate test
+echo "=============== migrate course ===============\n"
+python manage.py migrate course
+echo "=============== migrate challenge ===============\n"
+python manage.py migrate challenge
+
 echo "done ."
 echo "==============================================================="
 echo "================= collect static files ========================"
